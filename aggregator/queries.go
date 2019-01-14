154478bc2915b74db68f301b4065a9b3b9702d4f
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2019-01-14 11:42:48 +0100

    Add aggregator get entities

--- aggregator/queries.go
+++ aggregator/queries.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/iancoleman/strcase"
+	"github.com/jinzhu/inflection"
 	"github.com/machinebox/graphql"
 )
 
@@ -50,6 +51,41 @@ func GetEntity(options GetEntityOptions, res interface{}) error {
 	`, strcase.ToLowerCamel(options.Entity), strings.Join(options.Fields, " "))
 	req := graphql.NewRequest(query)
 	req.Var("id", options.EntityID)
+
+	return sendRequest(req, res)
+}
+
+// GetEntitiesOptions ...
+type GetEntitiesOptions struct {
+	Entity string
+	Fields []string
+	Filter map[string]interface{}
+	Sort   []string
+	Limit  *int
+	Offset *int
+}
+
+// GetEntities ...
+func GetEntities(options GetEntitiesOptions, res interface{}) error {
+	query := fmt.Sprintf(`
+		query ($filter: %sFilterType, $sort: [%sSortType], $limit: Int, $offset: Int) {
+			result: %s(filter:$filter,sort:$sort,limit:$limit,offset:$offset) {
+				items {
+					id %s
+				}
+				count
+			}
+		}
+	`, options.Entity, options.Entity, inflection.Plural(strcase.ToLowerCamel(options.Entity)), strings.Join(options.Fields, " "))
+	req := graphql.NewRequest(query)
+	req.Var("filter", options.Filter)
+	req.Var("sort", options.Sort)
+	if options.Offset != nil {
+		req.Var("offset", options.Offset)
+	}
+	if options.Limit != nil {
+		req.Var("limit", options.Limit)
+	}
 
 	return sendRequest(req, res)
 }
